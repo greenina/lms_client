246e3d33c3a759cae7771079b1455c285eba5d62
greenina <igreen0485@gmail.com>
2021-01-25 17:30:15 +0900

    feature:added password confirmation in register page

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -3,6 +3,7 @@ import './style.css'
 import axios from 'axios';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import {useAlert} from 'react-alert'; 
 
 
 class Register extends Component{
@@ -11,7 +12,8 @@ class Register extends Component{
     this.state={
       userId:'',
       userPassword :'',
-      isStudent :false
+      isStudent :false,
+      mode:false
     }
   }
   changeHandler =(e)=>{
@@ -19,15 +21,32 @@ class Register extends Component{
   }
 
   submitHandler = (e) =>{
-    e.preventDefault();
+    if(this.state.mode){
+      e.preventDefault();
     console.log(this.state)
     axios.post('http://192.249.18.245:8080/auth/register', this.state)
     .then(response=>{console.log(response)})
     .catch(error =>{
       console.log(error)
     })
+    alert('회원가입에 성공하셨습니다.')
+    }
+    else{
+      alert('비밀번호가 일치하지 않습니다')
+    }
+    
 
   }
+  passwordHandler = (e) =>{
+    e.preventDefault();
+    if(this.state.userPassword === e.target.value){
+      this.setState({mode:true})
+    }else{
+      this.setState({mode:false})
+    }
+  }
+
+  
 
   render(){
     const{userId, userPassword} = this.state
@@ -48,13 +67,22 @@ class Register extends Component{
               </Paper>
               <div className="blank1"></div>
               <Paper >
-              <div  align="left" className="userid">아이디</div>
+              <div  align="left" className="userid">id</div>
                 <input  onChange={this.changeHandler} value={userId} name="userId" className="userid-input" type="text"/>
               </Paper>
               <Paper >
-              <div  align="left" className="userid">비밀번호</div>
+              <div  align="left" className="userid">password</div>
                 <input  onChange={this.changeHandler} value={userPassword} name="userPassword" className="userPassword-input" type="text"/>
               </Paper>
+              <Paper >
+                <div  align="left" className="userid">confirm password</div>
+                <input  onChange={this.passwordHandler}  name="userPasswordConfirm" className="userPassword-input" type="text"/>
+                {this.state.mode?<div className="green">비밀번호가 일치합니다</div>:<div className="red">비밀번호가 일치하지 않습니다.</div>}
+              </Paper>
+              {/* <Paper >
+                <input type="radio">교수</input>
+                <input type="radio">학생</input>
+              </Paper> */}
 
               <button className = 'submit' onClick={this.submitHandler}>완료</button>
                 {/* <img className = 'submit' src = "/images/apply_button.png" height='50px' onClick={this.submitHandler}></img> */}
