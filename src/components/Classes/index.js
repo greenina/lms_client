7d525e5552591e33c22bb898778b9b6babfdc59b
greenina <igreen0485@gmail.com>
2021-01-26 18:52:17 +0900

    feature:inhwa is excited!!!!!

--- src/components/Classes/index.js
+++ src/components/Classes/index.js
@@ -1,17 +1,11 @@
-import React, { useState } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 import {useSelector, useDispatch} from "react-redux";
 import Modal from 'react-modal';
 import axios from 'axios';
 import ClassPage from '../ClassPage'
 import {Route} from 'react-router-dom'
 import {useHistory} from 'react-router-dom'
-
-const ClassInfo = (props) =>{
-    const classes = props.classes;
-    const classList = classes.map(element =><div>11121<li key={element[0].className}>{element[0].className},{element[0].instructor}</li></div>)
-    console.log("props",props.classes)
-    return(<ul>{classList}</ul>)
-}
+import { useAsync } from 'react-async';
 
 const Classes = () =>{
     const history = useHistory();
@@ -21,6 +15,8 @@ const Classes = () =>{
     var [className, setClassName] = useState();
     var [joinPassword, setJoinPassWord] = useState();
     var [lectureDate, setLectureDate] = useState();
+    var [classesInfo, setClassesInfo] = useState();
+
     var token = useSelector(state => {
         //console.log(state);
         return state.jwt}
@@ -83,33 +79,29 @@ const Classes = () =>{
             })
         }
       }
-    var classes = [];
     //var classesInfo = [];
-    axios.post('http://192.249.18.245:8080/class/get',{isStudent:isStudent,userId:userId},{
-        headers: {
-            'x-access-token': token
-        }
-    })
-    .then(res =>{
-        console.log(res)
-        if(res.data.success){
-            classes.push(res.data.classes);
-            //console.log(res.data.classes)
-            console.log(classes)
-            classes.forEach(element =>{
-                console.log("classes",classes)
-                console.log("element",element[0].className)
-                //classesInfo.push(<div>{element[0].className},{element[0].instructor}</div>)
-            })
-            //classesInfo = classes.map(element => <li key = {element[0].className}>{element[0].className},{element[0].instructor}</li>)
-        }
-        else{console.log(res.data.msg)}
-    })
-    .catch(error =>{
-        console.log(error)
+    const getDatafromServer = async() =>{
+        var res = await  axios.post('http://192.249.18.245:8080/class/get',{isStudent:isStudent,userId:userId},{
+            headers: {
+                'x-access-token': token
+            }
+        }) 
+        var classes = [];
+        classes.push(res.data.classes);
+        console.log(classes[0][0].className)
+        var info = classes.map(element => <li key = {element[0].className}>{element[0].className},{element[0].instructor}</li>)
+        setClassesInfo(info);
+        return info;
+    }
 
-    })
-    //professor's page
+    //var info = useAsync({promiseFn:getDatafromServer})
+    useEffect(()=>{
+        getDatafromServer();
+    },[])
+    
+
+    
+    
     const instructorHandler = (e) =>{
         setInstructor(e.target.value);
     }
@@ -127,7 +119,7 @@ const Classes = () =>{
     return(
         <div>
             <Route path="/classpage"exact={true} component={ClassPage}/>
-        {isStudent?<div>학생 수업 불러오기!!!<ClassInfo classes={classes}/></div>
+        {isStudent?<div>학생 수업 불러오기!!!{classesInfo}</div>
             :<div></div>}
             <button onClick={openModal} >Add Class</button>
             <Modal isOpen={modalState} onRequestClose={closeModal}>
@@ -151,5 +143,154 @@ const Classes = () =>{
         </div>
     );
 }
+
+// const Classes = () =>{
+//     const history = useHistory();
+//     var [modalState, setModalState] = useState(false);
+//     var [instructor, setInstructor] = useState(useSelector(state => state.userId));
+//     var [classId, setClassId] = useState();
+//     var [className, setClassName] = useState();
+//     var [joinPassword, setJoinPassWord] = useState();
+//     var [lectureDate, setLectureDate] = useState();
+//     var [classesInfo, setClassesInfo] = useState([]);
+//     var token = useSelector(state => {
+//         //console.log(state);
+//         return state.jwt}
+//     );
+//     var isStudent = useSelector(state => {
+//         console.log(state);
+//         return state.isStudent}
+//     );
+
+//     var userId = useSelector(state => {
+//         //console.log(state);
+//         return state.userId}
+//     );
+
+//     const openModal = () => {
+//         setModalState(true);
+//     };
+
+//     const closeModal = event => {
+//         event.preventDefault();
+//         setModalState(false);
+//     };
+
+//     const submitHandler = (e) =>{
+//         e.preventDefault();
+//         console.log("isStudent",isStudent)
+//         console.log(token)
+//         // debugger;
+//         if (!isStudent) {
+//             axios.post('http://192.249.18.245:8080/class/create', { userId: instructor, lectureDate: lectureDate, className: className, joinPassword: joinPassword },
+//                 {
+//                     headers: {
+//                         'x-access-token': token
+//                     }
+//                 })
+//                 .then(response => { console.log(response) })
+//                 .catch(error => {
+//                     console.log(error)
+//                 })
+//         }
+//         else{
+//             axios.post('http://192.249.18.245:8080/class/join', {userId:userId,joinPassword:joinPassword,className:className},
+//             {
+//                 headers: {
+//                     'x-access-token': token
+//                 }
+//             })
+//             .then(response=>{
+//                 console.log(response)
+//                 if(!response.data.success){
+//                     history.push('/classpage');
+//                 }else{
+//                     // document.location.href = "/classpage";
+//                     history.push('/classpage');
+//                 }
+//             })
+//             .catch(error =>{
+//                 return error
+//                 console.log(error)
+
+//             })
+//         }
+//       }
+//     var classes = [];
+    
+
+//     const getDataFromServer = async() =>{
+//         const res = await axios.post('http://192.249.18.245:8080/class/get',{isStudent:isStudent,userId:userId},{
+//             headers: {
+//                 'x-access-token': token
+//             }
+//         })
+//         await axios.post('http://192.249.18.245:8080/class/get',{isStudent:isStudent,userId:userId},{
+//             headers: {
+//                 'x-access-token': token
+//             }
+//         })
+//         return (res.data.classes).map(element => <li key = {element.className}>{element.className},{element.instructor}</li>)
+//     }
+    
+//     // const infos = useAsync({promiseFn:getDataFromServer});
+//     // setClassesInfo(infos)
+
+//     useEffect(()=>{
+//         const infos = useAsync({promiseFn:getDataFromServer})
+//         //const infos = await getDataFromServer();
+//         console.log("infos1",infos)
+//         setTimeout(() => {
+//             setClassesInfo(infos);
+//             console.log("infos2",infos);
+//         }, 3000);
+        
+        
+//      },[])
+//     //professor's page
+//     const instructorHandler = (e) =>{
+//         setInstructor(e.target.value);
+//     }
+//     const classIdHandler = (e) =>{
+//         setClassId(e.target.value);
+//     }
+//     const classNameHandler = (e) =>{
+//         setClassName(e.target.value);
+//     }
+//     const joinPasswordHandler = (e) =>{
+//         setJoinPassWord(e.target.value);
+//     }
+
+//     //student's page
+   
+//     return(
+//         <div>
+//             <Route path="/classpage"exact={true} component={ClassPage}/>
+//         {isStudent?<div>학생 수업 불러오기!!!{classesInfo}</div>
+//             :<div></div>}
+//             <button onClick={openModal} >Add Class</button>
+//             <Modal isOpen={modalState} onRequestClose={closeModal}>
+//                 {!isStudent?
+//                 <form onSubmit = {submitHandler}>
+//                     <div>lectureDate : <input onChange={function(e){
+//                         // var dateArr = [];
+//                         // dateArr.push(new Date(98,2,24))
+//                         setLectureDate("12/11/1981");
+//                     }}></input></div>
+//                     <div>className : <input onChange={classNameHandler}></input></div>
+//                     <div>joinPassword : <input  onChange={joinPasswordHandler}></input></div>
+//                     <button type="submit">수업 추가하기</button>
+//                 </form>:
+//                 <form onSubmit = {submitHandler}>
+//                     <div>className : <input onChange={classNameHandler}></input></div>
+//                     <div>joinPassword : <input  onChange={joinPasswordHandler}></input></div>
+//                     <button type="submit">수업 참가하기</button>
+//                 </form>}
+//             </Modal>
+//         </div>
+//     );
+
+    
+// }
 
 export default Classes;
