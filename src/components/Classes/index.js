85fc1fc349971423398ebbce5433d85aa292ecc5
greenina <igreen0485@gmail.com>
2021-01-26 21:04:52 +0900

    fix:classItem

--- src/components/Classes/index.js
+++ src/components/Classes/index.js
@@ -6,6 +6,7 @@ import ClassPage from '../ClassPage'
 import {Route} from 'react-router-dom'
 import {useHistory} from 'react-router-dom'
 import { useAsync } from 'react-async';
+import ClassItem from '../ClassItem'
 
 const Classes = () =>{
     const history = useHistory();
@@ -89,7 +90,7 @@ const Classes = () =>{
         var classes = [];
         classes.push(res.data.classes);
         console.log(classes[0][0].className)
-        var info = classes.map(element => <li key = {element[0].className}>{element[0].className},{element[0].instructor}</li>)
+        var info = classes.map(element => <ClassItem className={element[0].className} instructor = {element[0].instructor}/>)
         setClassesInfo(info);
         return info;
     }
@@ -119,7 +120,7 @@ const Classes = () =>{
     return(
         <div>
             <Route path="/classpage"exact={true} component={ClassPage}/>
-        {isStudent?<div>학생 수업 불러오기!!!{classesInfo}</div>
+        {isStudent?<div>수업 목록{classesInfo}</div>
             :<div></div>}
             <button onClick={openModal} >Add Class</button>
             <Modal isOpen={modalState} onRequestClose={closeModal}>
